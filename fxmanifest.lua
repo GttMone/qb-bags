8d012b0334e1a616e3c09848bbf4451e15778b36
Zerio <54480523+Z3rio@users.noreply.github.com>
2023-02-12 16:38:52 +0000

    Add `config.lua`

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -12,5 +12,6 @@ client_scripts {
 }
 
 server_scripts {
+    'config.lua',
     'server/main.lua',
 }
--- server/main.lua
+++ server/main.lua
@@ -1,5 +1,5 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-QBCore.Functions.CreateUseableItem('bag', function(source, item)
+QBCore.Functions.CreateUseableItem(Config.ItemName, function(source, item)
     TriggerClientEvent("qb-bags:client:openBag", source, item.info.serie)
 end)
